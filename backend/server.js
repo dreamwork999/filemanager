b75ffd428f781a252ff8c57ef78a09e0fb2e5cb9
nami1magine <nami@1magine.ca>
2023-01-06 12:04:57 +0900

    fourth commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,3 @@
-/**
- * @package		Cronus File Manager
- * @author		Farhad Aliyev Kanni
- * @copyright	Copyright (c) 2011 - 2019, Kannifarhad, Ltd. (http://www.kanni.pro/)
- * @license		https://opensource.org/licenses/GPL-3.0
- * @link		http://filemanager.kanni.pro
-**/
-
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
--- backend/utilits/appError.js
+++ backend/utilits/appError.js
@@ -1,11 +1,3 @@
-/**
- * @package		Cronus File Manager
- * @author		Farhad Aliyev Kanni
- * @copyright	Copyright (c) 2011 - 2019, Kannifarhad, Ltd. (http://www.kanni.pro/)
- * @license		https://opensource.org/licenses/GPL-3.0
- * @link		http://filemanager.kanni.pro
-**/
-
 class AppError extends Error {
     constructor(message, statusCode) {
       super(message);
